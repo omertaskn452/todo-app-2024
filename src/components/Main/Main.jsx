18a361897a7b5fadc2af5444260f834adbe163ae
omertaskn452 <omer_taskn@outlook.com>
2024-02-04 17:32:25 +0300

    Added some functions

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import style from "./main.css"
 import DeleteIcon from "../icons/DeleteIcon/DeleteIcon";
 import EditIcon from "../icons/EditIcon/EditIcon";
 import Todo from "../Todo/Todo";
+import { nanoid } from "nanoid";
 
 export default function Main() {
+
+  const  [todos, setTodos] = useState(JSON.parse(localStorage.getItem("Todos")) || [])
+
+  const [newTodoName, setNewTodoName] = useState("")
+
+  useEffect (() => {
+    localStorage.setItem("Todos", JSON.stringify(todos))
+  }, [todos])
+
+  const addingNewTodo = () => {
+    const date = new Date().toLocaleDateString()
+    const time = new Date().toLocaleTimeString()
+    setTodos(
+      [
+        ...todos,
+        {todoName: newTodoName, id: nanoid(), date: date, time: time}
+      ]
+    );
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(
+      todos.filter(todo => 
+        todo.id !== id)
+    )
+    console.log("todo was deleted")
+  }
+
+  let todoElements = todos.map((todo) => 
+    <Todo
+      name={todo.todoName}
+      id={todo.id}
+      date={todo.date}
+      time={todo.time}
+      deleteTodo={deleteTodo}
+    />  
+  )
+
   return(
     <div className="main">
       <h1>QU-TODO</h1>
@@ -15,14 +54,14 @@ export default function Main() {
             type="text" 
             name="" 
             id=""
+            value={newTodoName}
+            onChange={e => setNewTodoName(e.target.value)}
             placeholder="Add new todo" />
-          <button className="submit-button">ADD</button>
+            
+          <button onClick={addingNewTodo} className="submit-button">ADD</button>
         </div>
         <div className="todos">
-          <Todo/>
-          <Todo/>
-          <Todo/>
-          <Todo/>
+          {todoElements}
         </div>
       </div>
     </div>
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,19 +3,23 @@ import style from "./todo.css"
 import DeleteIcon from "../icons/DeleteIcon/DeleteIcon";
 import EditIcon from "../icons/EditIcon/EditIcon";
 
-export default function Todo() {
+export default function Todo(props) {
  
-  const date = new Date().toLocaleDateString();
+  const date = new Date().toLocaleTimeString();
 
   return(
     <div className="todo">
       <div className="todo-info">
-        <p className="todo-info-title">Hello World</p>
-        <span className="todo-date">{date}</span>
+        <p className="todo-info-title">{props.name}</p>
+        <span className="todo-date">{props.date} {props.time}</span>
       </div>
       <div className="todo-info-icons">
-        <EditIcon/>
-        <DeleteIcon/>  
+        <div>
+          <EditIcon/>
+        </div>
+        <div onClick={()=>props.deleteTodo(props.id)}>
+          <DeleteIcon/>  
+        </div>
       </div>
     </div>
   )
